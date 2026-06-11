39366400bacd3df31b6871e9fcac2315efc4ab07
Pasupathi <atpasupathi77@gmail.com>
2026-06-11 20:20:38 +0530

    Serve sitemap via serverless function with correct XML content-type

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,5 +1,6 @@
 export default function handler(req, res) {
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  res.setHeader('Content-Type', 'application/xml');
+  res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url><loc>https://tamilcalendar.vercel.app/</loc><lastmod>2026-06-11</lastmod><changefreq>daily</changefreq><priority>1.0</priority></url>
   <url><loc>https://tamilcalendar.vercel.app/daily</loc><lastmod>2026-06-11</lastmod><changefreq>daily</changefreq><priority>0.95</priority></url>
@@ -10,8 +11,5 @@ export default function handler(req, res) {
   <url><loc>https://tamilcalendar.vercel.app/holidays</loc><lastmod>2026-06-11</lastmod><changefreq>weekly</changefreq><priority>0.9</priority></url>
   <url><loc>https://tamilcalendar.vercel.app/chandrashtamam</loc><lastmod>2026-06-11</lastmod><changefreq>monthly</changefreq><priority>0.85</priority></url>
   <url><loc>https://tamilcalendar.vercel.app/about</loc><lastmod>2026-06-11</lastmod><changefreq>monthly</changefreq><priority>0.7</priority></url>
-</urlset>`;
-
-  res.setHeader('Content-Type', 'application/xml');
-  res.status(200).send(sitemap);
+</urlset>`);
 }
